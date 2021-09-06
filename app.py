5e05556a99fc60fc1e2bb6aa020a1eb81cc7ab66
NathanMLu <nathanlumax@Gmail.com>
2021-09-06 17:42:03 -0500

    debug

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if os.path.exists('config.py'):
 app = Flask(__name__)
 
 # Creates token file if it has not been created
-if os.path.exists('token'):
+if os.path.exists('lllllllaaammaaaaaseaaalll'):
     print('IT EXISTSTSTSTSTST')
     c = auth.client_from_token_file('token', config.apikey)
 else:
